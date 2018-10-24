5711a191aabc18dc54d7c1c3ae33dd9446ce8eec
vanyadelova <happy.vanya.delova@gmail.com>
2018-10-24 18:32:01 +0200

    running yarn test

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const board = [
     [null, null, null],
     [null, null, null]
   ]
+
+  function isValidBoard(board) {
+    // ...implement your logic here, return true or false
+  }
+  module.exports = {isValidBoard}
