0a5394967c596515ba43909e592d2844c1fc71c6
vanyadelova <happy.vanya.delova@gmail.com>
2018-10-26 14:40:20 +0200

    implement logic

--- main.js
+++ main.js
@@ -6,5 +6,38 @@ const board = [
 
   function isValidBoard(board) {
     // ...implement your logic here, return true or false
-  }
+  if (board.length === 3) {
+    const a = board.filter(c => c.length === 3);
+    if (a.length !== 3){
+      return false;
+    } else {
+      return checkCells(board);
+
+    }  
+  } else{ 
+    return false; 
+  }    
+}
+
+console.log(isValidBoard(board));
   module.exports = {isValidBoard}
+
+
+  
+
+
+  // ...implement your logic here, return true or false
+  function checkCells(board) {
+    const arrayCells = [true]
+
+    board.forEach(a => a.forEach(function(b) {
+        if (b !== null && b !== 'x' && b !== 'o') { arrayCells[0] = false }
+
+    }));
+    return arrayCells[0];
+}
+
+//console.log(checkCells(board));
+
+
+
--- main.spec.js
+++ main.spec.js
@@ -1,4 +1,4 @@
-const {isValidBoard} = require('./is-valid')
+const {isValidBoard} = require('./main')
 
 test('Board of 2x3 should be invalid', () => {
   const board = [
@@ -33,4 +33,13 @@ test('Board 3x3 with X and O should be valid', () => {
     [null, 'x', 'o']
   ]
   expect(isValidBoard(board)).toBe(true)
+})
+
+test('Boards that contain invalid symbols should be invalid', () => {
+  const board = [
+    [null, 'x', 'x'],
+    ['o', 'NONSENSE', null],
+    [null, 'x', 'I\'m a hacker!']
+  ]
+  expect(isValidBoard(board)).toBe(false)
 })
